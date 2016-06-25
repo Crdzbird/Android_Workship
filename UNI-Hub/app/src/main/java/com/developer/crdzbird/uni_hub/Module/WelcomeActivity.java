41cec07eae081afafe518ea69dc3791910b8dc95
Crdzbird <luisalfonsocb83@gmail.com>
2016-06-25 13:18:47 -0600

    Interfaz de logueo con nueva UI

---
+++ UNI-Hub/app/src/main/java/com/developer/crdzbird/uni_hub/Module/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.developer.crdzbird.uni_hub.Module;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.developer.crdzbird.uni_hub.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
